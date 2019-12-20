af366da491e87a01393a3795e83e100038a4e029
Amith Ananthram <amith.ananthram@gmail.com>
2019-12-20 06:53:57 +0000

    update meld evaluation code

--- egs/voxceleb/v2/evaluate_on_meld.sh
+++ egs/voxceleb/v2/evaluate_on_meld.sh
@@ -15,11 +15,24 @@ BASE_DIR="${meld_path}"
 # run extracted features through the nnet
 if [ $stage -eq 0 ]; then
 	echo "Stage 0: start"
-	mkdir -p "${output_path}/predictions"
-	for layers in seven_layers eight_layers; do
-		for mode in no_sil with_sil; do
-			for min_frame_len in 100 150 200 250 300; do 
-				model="${layers}_${mode}_${min_frame_len}"
+	# we begin by splitting the meld dataset
+	# so we can process the utterances 1-by-1
+	# (we do this because some of the utterances
+	# cause nnet3-compute-batch to hang....)
+	calling_directory=`pwd`	
+	split_directory=$meld_path/outputs/data/all_meld/splits
+	rm -rf $split_directory
+	mkdir -p $split_directory
+	cd $split_directory
+	split ../feats.scp -l 100 split
+	
+	cd $calling_directory
+	mkdir -p $output_path/predictions
+	# for layers in seven_layers eight_layers; do
+	#	for mode in no_sil with_sil; do
+	#		for min_frame_len in 100 150 200 250 300; do 
+	#			model="${layers}_${mode}_${min_frame_len}"
+				model="seven_layers_with_sil_250"
 				if [ ! -f "${model_path}/${model}.raw" ]
 				then
 					echo "${model} does not exist!"
@@ -30,28 +43,36 @@ if [ $stage -eq 0 ]; then
 					echo "${model} predictions exist, skipping"
 					continue
 				fi
-				nnet3-compute-batch "${model_path}/${model}.raw" scp:${output_path}/outputs/data/all_meld/feats.scp ark:${output_path}/predictions/${model}_prediction.ark
-			done
-		done
-	done
+				for split in `ls ${meld_path}/outputs/data/all_meld/splits`; do
+					(timeout 10s nnet3-compute-batch --use-gpu=wait "${model_path}/${model}.raw" scp:${meld_path}/outputs/data/all_meld/splits/${split} ark:${output_path}/predictions/${model}_${split}_prediction.ark || echo "$split timed out!") &>> ${output_path}/predictions/meld_${model}_predictions_log
+				done
+	#		done
+	#	done
+	# done
 	echo "Stage 0: end"
 fi
 
-# score nnet predictions against actual labels
 if [ $stage -eq 1 ]; then
-	echo "Stage 1: start"
+	model="seven_layers_with_sil_250"
+	splits=`ls ${output_path}/predictions/${model}*`
+	merge_emotion_prediction_results.py $splits ${output_path}/predictions/${model}_prediction.ark
+fi
+
+# score nnet predictions against actual labels
+if [ $stage -eq 2 ]; then
+	echo "Stage 2: start"
 	mkdir -p "${output_path}/scores"
-	for layers in seven_layers eight_layers; do
-		for mode in no_sil with_sil; do
-			for min_frame_len in 100 150 200 250 300; do
-				model="${layers}_${mode}_${min_frame_len}"
+	# for layers in seven_layers eight_layers; do
+	#	for mode in no_sil with_sil; do
+	#		for min_frame_len in 100 150 200 250 300; do
+				model="seven_layers_with_sil_250"
 				score_emotion_prediction_results.py \
 					$model \
 					"${output_path}/outputs/data/all_meld/utt2spk" \
 					"${output_path}/predictions/${model}_prediction.ark" \
 					"${output_path}/scores"
-			done
-		done
-	done
-	echo "Stage 1: end"
+	#		done
+	#	done
+	# done
+	echo "Stage 2: end"
 fi
