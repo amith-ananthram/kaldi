5429b292b0ed7e227966995fe62bc472749e4a32
Amith Ananthram <amith.ananthram@gmail.com>
2020-07-29 09:24:47 -0700

    more variants

--- egs/voxceleb/v2/lda-plda-emotion/run_lda_plda_experiments.sh
+++ egs/voxceleb/v2/lda-plda-emotion/run_lda_plda_experiments.sh
@@ -1,74 +1,264 @@
 # model 16, layer 6, speech only
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00000" \
+# 	--speech_dir models/cremad/16/xvectors/6/ \
+# 	--text_dir none \
+# 	--train_corpora cremad \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00001" \
+# 	--speech_dir models/cremad/16/xvectors/6/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap1 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00002" \
+# 	--speech_dir models/cremad/16/xvectors/6/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap2 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00003" \
+# 	--speech_dir models/cremad/16/xvectors/6/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap3 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00004" \
+# 	--speech_dir models/cremad/16/xvectors/6/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap4 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00005" \
+# 	--speech_dir models/cremad/16/xvectors/6/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap5 \
+# 	--output_dir lda_output
+# model 16, layer 7, speech only
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00006" \
+# 	--speech_dir models/cremad/16/xvectors/7/ \
+# 	--text_dir none \
+# 	--train_corpora cremad \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00007" \
+# 	--speech_dir models/cremad/16/xvectors/7/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap1 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00008" \
+# 	--speech_dir models/cremad/16/xvectors/7/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap2 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00009" \
+# 	--speech_dir models/cremad/16/xvectors/7/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap3 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00010" \
+# 	--speech_dir models/cremad/16/xvectors/7/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap4 \
+# 	--output_dir lda_output
+# lda-plda-emotion/lda_plda.sh \
+# 	--variant "00011" \
+# 	--speech_dir models/cremad/16/xvectors/7/ \
+# 	--text_dir none \
+# 	--train_corpora cremad,iemocap5 \
+# 	--output_dir lda_output
+
+# model 23, layer 6, speech only
 lda-plda-emotion/lda_plda.sh \
-	--variant "00000" \
-	--speech_dir models/cremad/16/xvectors/6/ \
+	--variant "00012" \
+	--speech_dir models/cremad/23/xvectors/6/ \
 	--text_dir none \
 	--train_corpora cremad \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00001" \
-	--speech_dir models/cremad/16/xvectors/6/ \
+	--variant "00013" \
+	--speech_dir models/cremad/23/xvectors/6/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap1 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00002" \
-	--speech_dir models/cremad/16/xvectors/6/ \
+	--variant "00014" \
+	--speech_dir models/cremad/23/xvectors/6/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap2 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00003" \
-	--speech_dir models/cremad/16/xvectors/6/ \
+	--variant "00015" \
+	--speech_dir models/cremad/23/xvectors/6/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap3 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00004" \
-	--speech_dir models/cremad/16/xvectors/6/ \
+	--variant "00016" \
+	--speech_dir models/cremad/23/xvectors/6/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap4 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00005" \
-	--speech_dir models/cremad/16/xvectors/6/ \
+	--variant "00017" \
+	--speech_dir models/cremad/23/xvectors/6/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap5 \
 	--output_dir lda_output
-# model 16, layer 7, speech only
+
+# model 23, layer 7, speech only
 lda-plda-emotion/lda_plda.sh \
-	--variant "00006" \
-	--speech_dir models/cremad/16/xvectors/7/ \
+	--variant "00018" \
+	--speech_dir models/cremad/23/xvectors/7/ \
 	--text_dir none \
 	--train_corpora cremad \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00007" \
-	--speech_dir models/cremad/16/xvectors/7/ \
+	--variant "00019" \
+	--speech_dir models/cremad/23/xvectors/7/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap1 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00008" \
-	--speech_dir models/cremad/16/xvectors/7/ \
+	--variant "00020" \
+	--speech_dir models/cremad/23/xvectors/7/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap2 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00009" \
-	--speech_dir models/cremad/16/xvectors/7/ \
+	--variant "00021" \
+	--speech_dir models/cremad/23/xvectors/7/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap3 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00010" \
-	--speech_dir models/cremad/16/xvectors/7/ \
+	--variant "00022" \
+	--speech_dir models/cremad/23/xvectors/7/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap4 \
 	--output_dir lda_output
 lda-plda-emotion/lda_plda.sh \
-	--variant "00011" \
-	--speech_dir models/cremad/16/xvectors/7/ \
+	--variant "00023" \
+	--speech_dir models/cremad/23/xvectors/7/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap5 \
+	--output_dir lda_output
+
+# model 23, layer 8, speech only
+lda-plda-emotion/lda_plda.sh \
+	--variant "00024" \
+	--speech_dir models/cremad/23/xvectors/8/ \
+	--text_dir none \
+	--train_corpora cremad \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00025" \
+	--speech_dir models/cremad/23/xvectors/8/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap1 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00026" \
+	--speech_dir models/cremad/23/xvectors/8/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap2 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00027" \
+	--speech_dir models/cremad/23/xvectors/8/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap3 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00028" \
+	--speech_dir models/cremad/23/xvectors/8/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap4 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00029" \
+	--speech_dir models/cremad/23/xvectors/8/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap5 \
+	--output_dir lda_output
+
+# model 3, layer 6, speech only
+lda-plda-emotion/lda_plda.sh \
+	--variant "00030" \
+	--speech_dir models/cremad/3/xvectors/6/ \
+	--text_dir none \
+	--train_corpora cremad \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00031" \
+	--speech_dir models/cremad/3/xvectors/6/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap1 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00032" \
+	--speech_dir models/cremad/3/xvectors/6/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap2 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00033" \
+	--speech_dir models/cremad/3/xvectors/6/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap3 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00034" \
+	--speech_dir models/cremad/3/xvectors/6/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap4 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00035" \
+	--speech_dir models/cremad/3/xvectors/6/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap5 \
+	--output_dir lda_output
+
+# model 3, layer 7, speech only
+lda-plda-emotion/lda_plda.sh \
+	--variant "00036" \
+	--speech_dir models/cremad/3/xvectors/7/ \
+	--text_dir none \
+	--train_corpora cremad \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00037" \
+	--speech_dir models/cremad/3/xvectors/7/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap1 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00038" \
+	--speech_dir models/cremad/3/xvectors/7/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap2 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00039" \
+	--speech_dir models/cremad/3/xvectors/7/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap3 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00040" \
+	--speech_dir models/cremad/3/xvectors/7/ \
+	--text_dir none \
+	--train_corpora cremad,iemocap4 \
+	--output_dir lda_output
+lda-plda-emotion/lda_plda.sh \
+	--variant "00041" \
+	--speech_dir models/cremad/3/xvectors/7/ \
 	--text_dir none \
 	--train_corpora cremad,iemocap5 \
 	--output_dir lda_output
