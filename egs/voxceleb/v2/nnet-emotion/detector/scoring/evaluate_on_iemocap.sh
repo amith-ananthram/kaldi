7a990e1158ea0dc237057503fe9ec839576c762b
Amith Ananthram <amith.ananthram@gmail.com>
2020-08-15 18:43:53 -0400

    update evaluation script

--- egs/voxceleb/v2/nnet-emotion/detector/scoring/evaluate_on_iemocap.sh
+++ egs/voxceleb/v2/nnet-emotion/detector/scoring/evaluate_on_iemocap.sh
@@ -7,29 +7,28 @@ set -e
 
 stage=placeholder
 model_path=placeholder
-corpus_path=placeholder
-features_path=placeholder
-
 chunk_size=placeholder
+target_emotions_mode=placeholder
+target_emotions_config=placeholder
 test_set=all_iemocap
 
 . ./utils/parse_options.sh
 
-CSVS_DIR="${corpus_path}/csvs"
-DATA_DIR="${corpus_path}/IEMOCAP_full_release"
-
-MFCC_DIR="${features_path}/mfcc"
+BASE_DIR="evaluate"
+MFCC_DIR="${BASE_DIR}/mfcc"
 
 # prepare the corpus for feature extraction
 if [ $stage -eq 0 ]; then
 	echo "Stage $stage: start"
 	for session in 1 2 3 4 5; do
-		session_output_path="${features_path}/session${session}"
+		session_output_path="${BASE_DIR}/session${session}"
 		mkdir -p $session_output_path
 		nnet-emotion/detector/scoring/generate_iemocap_inputs.py \
-			"${CSVS_DIR}/Session${session}.csv" \
-			"${DATA_DIR}/Session${session}/sentences/wav" \
-			$session_output_path
+			--test_corpus "iemocap${session}" \
+			--test_corpus_config "all" \
+			--target_emotions_mode $target_emotions_mode \
+			--target_emotions_config $target_emotions_config \
+			--output_dir $session_output_path
 		utils/utt2spk_to_spk2utt.pl "${session_output_path}/utt2spk" > "${session_output_path}/spk2utt"
 	done
 	echo "Stage $stage: end"
@@ -39,15 +38,15 @@ fi
 if [ $stage -eq 1 ]; then
 	echo "Stage $stage: start"
 	for session in 1 2 3 4 5; do
-		session_input_path="${features_path}/session${session}" 
+		session_input_path="${BASE_DIR}/session${session}" 
 		steps/make_mfcc_pitch.sh --write-utt2num-frames true --mfcc-config conf/mfcc.conf --pitch-config conf/pitch.conf --nj 40 --cmd "$train_cmd" \
-			$session_input_path ${features_path}/exp/make_mfcc $MFCC_DIR
+			$session_input_path ${BASE_DIR}/exp/make_mfcc $MFCC_DIR
 		utils/fix_data_dir.sh $session_input_path
 		sid/compute_vad_decision.sh --nj 40 --cmd "$train_cmd" \
-      		$session_input_path ${features_path}/exp/make_vad $MFCC_DIR
+      		$session_input_path ${BASE_DIR}/exp/make_vad $MFCC_DIR
       	utils/fix_data_dir.sh $session_input_path
 	done
-	utils/combine_data.sh "${features_path}/all_iemocap" "${features_path}/session1" "${features_path}/session2" "${features_path}/session3" "${features_path}/session4" "${features_path}/session5"
+	utils/combine_data.sh "${BASE_DIR}/all_iemocap" "${BASE_DIR}/session1" "${BASE_DIR}/session2" "${BASE_DIR}/session3" "${BASE_DIR}/session4" "${BASE_DIR}/session5"
 	echo "Stage $stage: end"
 fi
 
@@ -55,7 +54,7 @@ fi
 if [ $stage -eq 2 ]; then
 	echo "Stage $stage: generating predictions for specified model"
 	mkdir -p "${model_path}/predictions"
-	nnet3-xvector-compute-batched --use-gpu=yes --chunk-size=$chunk_size "${model_path}/final.raw" scp:${features_path}/$test_set/feats.scp ark:${model_path}/predictions/iemocap_predictions.ark
+	nnet3-xvector-compute-batched --use-gpu=yes --chunk-size=$chunk_size "${model_path}/final.raw" scp:${BASE_DIR}/$test_set/feats.scp ark:${model_path}/predictions/iemocap_predictions.ark
 	echo "Stage $stage: end"
 fi
 
@@ -64,7 +63,7 @@ if [ $stage -eq 3 ]; then
 	echo "Stage $stage: generating scores for specified model"
 	mkdir -p "${model_path}/scores"
 	nnet-emotion/detector/scoring/score_emotion_prediction_results.py \
-		"${features_path}/$test_set/utt2spk" \
+		"${BASE_DIR}/$test_set/utt2spk" \
 		"${model_path}/predictions/iemocap_predictions.ark" \
 		"${model_path}/scores/iemocap_scores.txt"
 	echo "Stage $stage: end"
--- egs/voxceleb/v2/nnet-emotion/detector/scoring/generate_iemocap_inputs.py
+++ egs/voxceleb/v2/nnet-emotion/detector/scoring/generate_iemocap_inputs.py
@@ -2,23 +2,27 @@
 
 # author: aa4461
 
-import re
+import os
 import csv
-import sys
-import os.path
+import glob
+import argparse
+import numpy as np
+from bidict import bidict
+
+IEMOCAP_DIR = 'corpora/iemocap/'
+IEMOCAP_WAV_DIR = os.path.join(IEMOCAP_DIR, 'IEMOCAP_full_release')
+IEMOCAP_INPUT_CSV_DIR = os.path.join(IEMOCAP_DIR, 'csvs')
+
+IEMOCAP_EMOTIONS = bidict({emo: emo for emo in ['ang', 'dis', 'exc', 'fea', 'fru', 'hap', 'neu', 'sad', 'sur']})
+IEMOCAP_DIALOGUE_TYPES = {'improv': ['improvisation'], 'script': ['script'], 'all': ['improvisation', 'script']}
+
+SUPPORTED_TARGET_EMOTION_MODES = {'select', 'collapse'}
 
 UTT2SPK_FILE = 'utt2spk'
 WAV_FILE = 'wav.scp'
 
-COLLAPSED_IEMOCAP_EMOTIONS = {
-	'0': 'anger/disgust',
-	'1': 'fear/surprise',
-	'2': 'happiness',
-	'3': 'neutral',
-	'4': 'sadness'
-}
-
-class UtteranceDetails:
+# TODO: share between generate_iemocap_inputs and generate_corpora_inputs
+class IemocapUtteranceDetails:
 	def __init__(self, session, mocap_source, dialogue_type, 
 		dialogue_number, utterance_number, utterance, speaker,
 		src_file, orig_emotion, mapped_emotion):
@@ -33,13 +37,9 @@ class UtteranceDetails:
 		self.orig_emotion = orig_emotion
 		self.mapped_emotion = mapped_emotion
 
-	def __repr__(self):
-		return "%s: %s" % (self.get_filename(), self.utterance)
-
-	# old id format (might be required for nnet scoring)
+	# old id format (required to find WAV files)
 	def get_old_id(self):
-		return "%s_Ses%s%s_%s%s_%s%s" % (
-			self.orig_emotion,
+		return "Ses%s%s_%s%s_%s%s" % (
 			self.session,
 			self.mocap_source,
 			self.get_dialogue_type_shortname(),
@@ -54,6 +54,9 @@ class UtteranceDetails:
 			self.mapped_emotion, self.src_file, self.session, self.mocap_source, 
 			self.dialogue_type, self.dialogue_number, self.utterance_number, self.speaker)
 
+	def get_emotion(self):
+		return self.mapped_emotion
+
 	def get_dialogue_type_shortname(self):
 		if self.dialogue_type == "improvisation":
 			return "impro"
@@ -63,98 +66,136 @@ class UtteranceDetails:
 			raise Exception("Unsupported dialogue type: %s" % (self.dialogue_type))
 
 	# example filename: Ses01F_impro01/Ses01F_impro01_F000.wav
-	def get_filename(self):
-		return "Ses%s%s_%s%s/%s.wav" % (
+	def get_wav_filename(self):
+		wav_dir = os.path.join(IEMOCAP_WAV_DIR, 'Session%s/sentences/wav' % (int(self.session)))
+		return os.path.join(wav_dir, "Ses%s%s_%s%s/%s.wav" % (
 			self.session,
 			self.mocap_source,
 			self.get_dialogue_type_shortname(),
 			self.dialogue_number,
-			self.get_old_id()[2:]
-		)
+			self.get_old_id()
+		))
 
-def get_utterances(input_csv):
-	src_file = None
-	if input_csv.find("train") != -1:
-		src_file = "train"
-	elif input_csv.find("dev") != -1:
-		src_file = "dev"
-	else:
-		src_file = "test"
-
+# TODO: share between generate_iemocap_inputs and generate_corpora_inputs
+def get_iemocap_utterances(subsets, config, emotion_mapper):
 	utterances = []
-	with open(input_csv) as f:
-		csv_reader = csv.reader(f, delimiter=',')
-		line_count = 0
-		for row in csv_reader:
-			if line_count == 0:
-				line_count += 1
-				continue
-			else:
-				# example CSV row
-				# 0 Sr.No: 1303
-				# 1 Session_Number: 01
-				# 2 Mocap_Source: F
-				# 3 Dialogue_Type: improvisation
-				# 4 Dialogue_Number: 01
-				# 5 Utterance_Number: 000
-				# 6 StartTime: 6.2901
-				# 7 EndTime: 8.2357
-				# 8 Utterance: Excuse me.
-				# 9 Speaker: F
-				# 10 Emotion: neu
-				# 11 Emotion_Label: 3
-				session = row[1]
-				mocap_source = row[2]
-				dialogue_type = row[3]
-				dialogue_number = row[4]
-				utterance_number = row[5]
-				utterance = row[8]
-				speaker = row[9]
-				orig_emotion = row[11]
-				mapped_emotion = COLLAPSED_IEMOCAP_EMOTIONS[orig_emotion]
-				utterances.append(
-					UtteranceDetails(
-						session,
-						mocap_source,
-						dialogue_type,
-						dialogue_number,
-						utterance_number,
-						utterance,
-						speaker,
-						src_file,
-						orig_emotion,
-						mapped_emotion
+	for subset in subsets:
+		input_csv = os.path.join(IEMOCAP_INPUT_CSV_DIR, 'Session%s.csv' % (subset))
+
+		src_file = None
+		if input_csv.find("train") != -1:
+			src_file = "train"
+		elif input_csv.find("dev") != -1:
+			src_file = "dev"
+		else:
+			src_file = "test"
+
+		with open(input_csv) as f:
+			csv_reader = csv.reader(f, delimiter=',')
+			line_count = 0
+			for row in csv_reader:
+				if line_count == 0:
+					line_count += 1
+					continue
+				else:
+					# example CSV row
+					# 0 Sr.No: 1303
+					# 1 Session_Number: 01
+					# 2 Mocap_Source: F
+					# 3 Dialogue_Type: improvisation
+					# 4 Dialogue_Number: 01
+					# 5 Utterance_Number: 000
+					# 6 StartTime: 6.2901
+					# 7 EndTime: 8.2357
+					# 8 Utterance: Excuse me.
+					# 9 Speaker: F
+					# 10 Emotion: neu
+					session = row[1]
+					mocap_source = row[2]
+					dialogue_type = row[3]
+					dialogue_number = row[4]
+					utterance_number = row[5]
+					utterance = row[8]
+					speaker = row[9]
+					orig_emotion = row[10]
+
+					if int(session) != int(subset):
+						raise Exception("Unexpectedly found %s in CSV for %s" % (session, subset)) 
+
+					if config != 'all' and dialogue_type != IEMOCAP_DIALOGUE_TYPES[config]:
+						continue
+
+					if orig_emotion not in emotion_mapper:
+						continue
+
+					mapped_emotion = emotion_mapper[orig_emotion]
+					utterances.append(
+						IemocapUtteranceDetails(
+							session,
+							mocap_source,
+							dialogue_type,
+							dialogue_number,
+							utterance_number,
+							utterance,
+							speaker,
+							src_file,
+							orig_emotion,
+							mapped_emotion
+						)
 					)
-				)
-				line_count += 1
+					line_count += 1
 	return utterances
 
-# note that in the utt2spk file we generate we map each 
-# utterance to a numerical encoding of the utterance's 
-# emotion label according to IEMOCAP (doesn't matter thaaat much)
-def generate_utt2spk(utterances, output_data_dir):
+# TODO: share between generate_iemocap_inputs and generate_corpora_inputs
+def generate_utt2spk(emotion_mapper, utterances, output_data_dir):
+	emotions_to_id = {emotion:idx for idx, emotion in enumerate(sorted(emotion_mapper.values()))}
 	with open(os.path.join(output_data_dir, UTT2SPK_FILE), 'w') as f:
 		for utterance in sorted(utterances, key=lambda utterance: utterance.get_id()):
-			f.write("%s %s\n" % (utterance.get_id(), utterance.orig_emotion))
+			f.write("%s %s\n" % (utterance.get_id(), emotions_to_id[utterance.get_emotion()]))
 
-def generate_wavscp(utterances, input_data_dir, output_data_dir):
+# TODO: share between generate_iemocap_inputs and generate_corpora_inputs
+def generate_wavscp(utterances, output_data_dir):
 	with open(os.path.join(output_data_dir, WAV_FILE), 'w') as f:
 		for utterance in sorted(utterances, key=lambda utterance: utterance.get_id()):
-			wav_file_path = os.path.join(input_data_dir, utterance.get_filename())
-			if not os.path.exists(wav_file_path):
-				raise Exception('Missing file: %s' % (utterance.get_filename()))
-			wav_creation_cmd = "ffmpeg -v 8 -i %s -f wav -ar 16000 -acodec pcm_s16le -|" % (wav_file_path)
+			if not os.path.exists(utterance.get_wav_filename()):
+				raise Exception('Missing file: %s' % (utterance.get_wav_filename()))
+			wav_creation_cmd = "ffmpeg -v 8 -i %s -f wav -ar 16000 -acodec pcm_s16le -|" % (utterance.get_wav_filename())
 			f.write("%s %s\n" % (utterance.get_id(), wav_creation_cmd))
 
-def main():
-	input_csv = sys.argv[1]
-	input_data_dir = sys.argv[2]
-	output_data_dir = sys.argv[3]
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Generate Kaldi input files based on specified train corpora and target configs.')
+	parser.add_argument('--test_corpus', dest='test_corpus', help='The test corpus to use (iemocap{1|2|3|4|5})', required=True)
+	parser.add_argument('--test_corpus_config', dest='test_corpus_config', help='How to select examples from the test corpus.', required=True)
+	parser.add_argument('--target_emotions_mode', dest='target_emotions_mode', help='\'select\' or \'collapse\'', required=True)
+	parser.add_argument('--target_emotions_config', dest='target_emotions_config', help='How to select or collapse labels.', required=True)
+	parser.add_argument('--output_dir', dest='output_dir', help='Where to place generated utt2spk and wav.scp files', required=True)
+	args = parser.parse_args()
 
-	utterances = get_utterances(input_csv)
+	if 'iemocap' not in args.test_corpus:
+		raise Exception("Unsupported test_corpus: %s" % (test_corpus))
 
-	generate_utt2spk(utterances, output_data_dir)
-	generate_wavscp(utterances, input_data_dir, output_data_dir)
+	target_emotions_mode = args.target_emotions_mode
+	if target_emotions_mode not in SUPPORTED_TARGET_EMOTION_MODES:
+		raise Exception("Unsupported target_emotions_mode")
 
-if __name__ == "__main__":
-	main()
+	emotion_mapper = {}
+	target_emotion_config = args.target_emotions_config.split(',')
+	for config_element in target_emotion_config:
+		if '/' in config_element:
+			emotions = config_element.split('/')
+		else:
+			emotions = [config_element]
+		for emotion in emotions:
+			assert emotion in IEMOCAP_EMOTIONS
+
+		if target_emotions_mode == 'select':
+			emotion_mapper[emotions[0]] = emotions[0]
+		else:
+			collapsed_label = '/'.join(emotions)
+			for emotion in emotions:
+				emotion_mapper[emotion] = collapsed_label
+
+	utterances = get_iemocap_utterances([int(args.test_corpus[-1:])], args.test_corpus_config, emotion_mapper)
+
+	generate_utt2spk(emotion_mapper, utterances, args.output_dir)
+	generate_wavscp(utterances, args.output_dir)
